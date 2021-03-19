0ee82659cbf21667f34da5ab0f6ae2d79f6ef051
maartendriessens <maartenpoldriessens@gmail.com>
2021-03-19 13:27:39 +0100

    Version 19/03/2021

---
+++ app/src/main/java/com/example/pingpongrobot/MainActivity.java
@@ -0,0 +1,126 @@
+package com.example.pingpongrobot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static SeekBar seekBarSpeed;
+    private static TextView textSeekBar;
+    private static ToggleButton toggleMode;
+    private PrintWriter output;
+    private BufferedReader input;
+    private int speed = 1;
+    private boolean connected = false;
+    private static final int SERVER_PORT = 3333;
+    private static final String SERVER_IP = "192.168.4.1";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toggleMode =  (ToggleButton) findViewById(R.id.toggleMode);
+        Bundle extras = getIntent().getExtras();
+        if (extras == null) {
+            new Thread(new threadConnected()).start();
+        }
+        else{
+            speed = extras.getInt("speed");
+            connected = true;
+        }
+        seekbar();
+    }
+
+    public static class SocketHandler {
+        private static Socket socket;
+        public static synchronized Socket getSocket(){ return socket; }
+        public static synchronized void setSocket(Socket socket){ SocketHandler.socket = socket; }
+    }
+
+    class threadConnected implements Runnable {
+        @Override
+        public void run() {
+            try {
+                Socket socket = new Socket(SERVER_IP, SERVER_PORT);
+                SocketHandler.setSocket(socket);
+                output = new PrintWriter(socket.getOutputStream());
+                input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), "Connected to: " + SERVER_IP + " " + SERVER_PORT , Toast.LENGTH_LONG).show();
+                        connected = true;
+                    }
+                });
+                //new Thread(new SessionActivity.threadIn()).start();
+            } catch (UnknownHostException e1) {
+                e1.printStackTrace();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+
+    public void seekbar(){
+        seekBarSpeed = (SeekBar) findViewById(R.id.seekBarSpeed);
+        textSeekBar = (TextView) findViewById(R.id.textSeekBar);
+        textSeekBar.setText("#balls/minute = " + seekBarSpeed.getProgress());
+        seekBarSpeed.setProgress(0);
+        seekBarSpeed.incrementProgressBy(speed);
+        textSeekBar.setText("#balls/minute = " + speed);
+        seekBarSpeed.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    int progress_value;
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        progress_value = progress + 1;
+                        textSeekBar.setText("#balls/minute = " + progress); }
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) { }
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) { textSeekBar.setText("#balls/minute = " + progress_value); }
+                }
+        );
+    }
+
+
+    public void onPlayButton( View caller)
+    {
+        if (connected) {
+            Intent intent = new Intent(this, SessionActivity.class);
+            intent.putExtra("mode", toggleMode.getText().toString().toUpperCase());
+            intent.putExtra("speed", seekBarSpeed.getProgress()+1);
+            startActivity(intent);
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "Not connected to ESP" , Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void onHistoryButton( View caller)
+    {
+        Intent intent = new Intent(this, HistoryActivity.class);
+        startActivity(intent);
+    }
+
+    public void onSettingsButton( View caller)
+    {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/pingpongrobot/SessionActivity.java
@@ -0,0 +1,160 @@
+package com.example.pingpongrobot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class SessionActivity extends AppCompatActivity {
+    private PrintWriter output;
+    private BufferedReader input;
+    private enum Mode {AUTOMATIC, MANUAL};
+    private Mode mode;
+    private TextView textTitle, textSpeed,textCounter;
+    private Chronometer timer;
+    private Button ButtonstartStop, buttonRight, buttonLeft;
+    private int speed, counter = 0;
+    private long lastTime;
+    private Boolean playing = false;
+    private Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_session);
+
+        textTitle = (TextView) findViewById(R.id.textTitle);
+        textSpeed = (TextView) findViewById(R.id.textSpeed2);
+        textCounter = (TextView) findViewById(R.id.textCounter);
+        timer = (Chronometer) findViewById(R.id.textTimer);
+        ButtonstartStop = (Button) findViewById(R.id.buttonStartStop);
+        buttonLeft = (Button) findViewById(R.id.buttonLeft);
+        buttonRight = (Button) findViewById(R.id.buttonRight);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String modeString = extras.getString("mode");
+            mode = Mode.valueOf(modeString);
+            speed = extras.getInt("speed");
+        }
+        textTitle.setText(mode.toString());
+        textSpeed.setText(String.valueOf(speed));
+        Socket socket = MainActivity.SocketHandler.getSocket();
+        try {
+            output = new PrintWriter(socket.getOutputStream());
+            input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch (UnknownHostException e1) {
+            e1.printStackTrace();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+        switch (mode) {
+            case AUTOMATIC:
+                buttonLeft.setVisibility(View.INVISIBLE);
+                buttonRight.setVisibility(View.INVISIBLE);
+        }
+
+    }
+
+    class threadIn implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    final String message = input.readLine();
+                    if (message != null) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(getApplicationContext(), "Input: " + message, Toast.LENGTH_LONG).show();
+                            }
+                        });
+                    } else {
+                        //new Thread(new threadConnected()).start();
+                        return;
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    class threadOut implements Runnable {
+        private String message;
+        threadOut(String message) {
+            this.message = message;
+        }
+        @Override
+        public void run() {
+            output.write(message);
+            output.flush();
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    
+                }
+            });
+        }
+    }
+
+    public void onStartStopButton( View caller)
+    {
+        if (playing == false){ startSession();}
+        else{ stopSession();}
+    }
+
+    public void startSession(){
+        if (lastTime != 0){
+            timer.setBase(timer.getBase() + SystemClock.elapsedRealtime() - lastTime);
+        }
+        else{
+            timer.setBase(SystemClock.elapsedRealtime());
+        }
+        timer.start();
+        ButtonstartStop.setText("Stop");
+        playing = true;
+        signalRunnable.run();
+    }
+
+    public void stopSession(){
+        lastTime = SystemClock.elapsedRealtime();
+        timer.stop();
+        ButtonstartStop.setText("Start");
+        playing = false;
+        handler.removeCallbacks(signalRunnable);
+    }
+
+    private Runnable signalRunnable = new Runnable() {
+        @Override
+        public void run() {
+            new Thread(new threadOut("TOGGLE")).start();
+            counter++;
+            textCounter.setText(String.valueOf(counter));
+            handler.postDelayed(signalRunnable,60000/speed);
+        }
+    };
+
+    public void onButBack(View caller){
+        stopSession();
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("speed", speed);
+        startActivity(intent);
+    }
+
+}
